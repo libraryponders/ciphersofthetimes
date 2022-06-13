1dcf4c95934eb21859ae6d3fc760af5cc029da9d
libraryponders <libraryponders@gmail.com>
2022-06-13 14:00:29 +0300

    updates to novel paths and added sample newspapers option

--- utilities/newspaper_corpus_processor/cleaning_functions.py
+++ utilities/newspaper_corpus_processor/cleaning_functions.py
@@ -23,16 +23,7 @@ regex_expressions = {"initials": r"\b([A-Z][.](\s)?)+", "prefixes": r"(Mr|St|Mrs
                      "pennies": r"(\d+[.]?\s*)[d][.]", "months_and_years": r"\d{1,2}[.]\s*(\d{4})"}
 
 
-### Takes in a path and returns a list of tuples in the format (FILE_NAME, TEXT)
-def input_corpus_of_txts(path):
-    list_of_filenames_and_dirty_texts = []
-    for filename in os.listdir(path):
-        # double check that these are text files
-        if filename.endswith(".txt"):
-            with codecs.open(path + filename, 'r', encoding='utf-8', errors="ignore") as raw_text:
-                dirty_text = raw_text.read()
-            list_of_filenames_and_dirty_texts.append((filename, dirty_text))
-    return list_of_filenames_and_dirty_texts
+
 
 ### Strips all accented characters in the text
 def strip_accents(text):
@@ -74,26 +65,34 @@ def process_pennies(matchobj):
     return text
 
 ### This function calls all the preprocessing functions from above
-def preprocess_text(text):
-    # remove all the line breaks created by newspaper processor
-    text = re.sub(regex_expressions["line_break"],"", text)
-    # marking initials:
-    text = re.sub(regex_expressions["initials"], process_initials, text)
-    # process titles:
-    text = re.sub(regex_expressions["prefixes"],"\\1<prd>", text, flags=re.IGNORECASE)
-    # process month abbreviations:
-    text = re.sub(regex_expressions["months_abrv"], process_months_abrv, text, flags=re.IGNORECASE)
-    # process instances of months [period] year:
-    text = re.sub(regex_expressions["months_and_years"], process_periods_to_commas, text)
-    # process instances of "No."
-    text = re.sub(r"(No|Nos)[.]","number", text, flags=re.IGNORECASE)
-    # strip all dashes:
-    text = re.sub(regex_expressions["dashes"], " ", text)
-    # transform all quotes to ' " ':
-    text = re.sub(regex_expressions["quote_marks"], '"', text)
-    # strip all pennies "XX d." in the text:
-    text = re.sub(regex_expressions["pennies"], process_pennies, text)
-    # strip all accents from the text:
+def preprocess_text(text, category=0):
+    # newspapers
+    if category == 0:
+        # remove all the line breaks created by newspaper processor
+        text = re.sub(regex_expressions["line_break"],"", text)
+        # marking initials:
+        text = re.sub(regex_expressions["initials"], process_initials, text)
+        # process titles:
+        text = re.sub(regex_expressions["prefixes"],"\\1<prd>", text, flags=re.IGNORECASE)
+        # process month abbreviations:
+        text = re.sub(regex_expressions["months_abrv"], process_months_abrv, text, flags=re.IGNORECASE)
+        # process instances of months [period] year:
+        text = re.sub(regex_expressions["months_and_years"], process_periods_to_commas, text)
+        # process instances of "No."
+        text = re.sub(r"(No|Nos)[.]","number", text, flags=re.IGNORECASE)
+        # strip all dashes:
+        text = re.sub(regex_expressions["dashes"], " ", text)
+        # transform all quotes to ' " ':
+        # Not sure if quote marks should be excised
+        # text = re.sub(regex_expressions["quote_marks"], '"', text)
+        # strip all pennies "XX d." in the text:
+        text = re.sub(regex_expressions["pennies"], process_pennies, text)
+        # strip all accents from the text:
+    # category == 1 is novels
+    else:
+        # only process titles for novels
+        text = re.sub(regex_expressions["prefixes"],"\\1<prd>", text, flags=re.IGNORECASE)
+    # strip accents for all texts
     text = strip_accents(text)
     return text
 
@@ -112,11 +111,10 @@ def clean_tokenized_sent(sent):
     
     # transform all the words that are completely uppercase to lowercase
     for index, word in enumerate(split_sentence):
-        if (word.isupper()):
+        if (word.isupper()) and (len(word) > 1):
             new_word = word.lower()
             split_sentence[index] = new_word
     clean_sent = " ".join(split_sentence)
-    
     # put back the periods:
     clean_sent = re.sub("<prd>", ".", clean_sent)
     # clean_sent = clean_sent.lower()
@@ -135,39 +133,49 @@ def clean_tokenized_list(sent_list):
 ### It takes the output of input_corpus_of_txts() (a tuple of (FILE_NAME, TEXT)),
 ### applies preprocessing functions and outputs a pandas dataframe of the processed
 ### texts originally supplied to the function. 
-def process_dirty_texts_to_df(list_of_filenames_and_dirty_texts):
+def process_dirty_texts_to_df(corpora_of_filenames_and_dirty_texts):
     # creating an empty list to hold all the information from processing
     cleaned_texts = []
     # empty dictionary to transform into pandas dataframe
     cleaned_corpus_as_dictionary = {}
     # iterates over every file supplied
-    count=0
-    for filename, dirty_text in list_of_filenames_and_dirty_texts:
-        progress(count, len(list_of_filenames_and_dirty_texts))
-        # preprocesses the text
-        preprocessed_text = preprocess_text(dirty_text)
-        # tokenizes the text into sentences
-        tokenized_sentences = sent_tokenize(preprocessed_text)
-        # cleans the list of tokenized sentences
-        cleaned_tokenized_sentences = clean_tokenized_list(tokenized_sentences)
-        # setting up a relative_sentence_index to capture the location
-        # of each sentence within their respective files
-        relative_sentence_index = 0
-        # iterate over every individual sentence within the cleaned and tokenized sentence
-        # (each sentence in a single document)
-        for clean_tokenized_sentence in cleaned_tokenized_sentences:
-            # creates a tuple of all the information we'd like to see in the dataframe
-            tupled_files = (filename, clean_tokenized_sentence, relative_sentence_index)
-            # adds them to our empty list above (cleaned_texts)
-            cleaned_texts.append(tupled_files)
-            # increments the relative sentence index
-            relative_sentence_index += 1
-        count += 1
+    for index, corpus in enumerate(corpora_of_filenames_and_dirty_texts):
+        count = 0
+        if index == 0:
+            corpus_name = "Newspaper Corpus"
+        elif index == 1:
+            corpus_name = "Novels Corpus"
+        print(f"[?] There are {len(corpus)} texts to process in {corpus_name}")
+        for filename, dirty_text in corpus:
+            progress(count, len(corpus))
+            # preprocesses the text
+            preprocessed_text = preprocess_text(dirty_text, index)
+            # tokenizes the text into sentences
+            tokenized_sentences = sent_tokenize(preprocessed_text)
+            # cleans the list of tokenized sentences
+            cleaned_tokenized_sentences = clean_tokenized_list(tokenized_sentences)
+            # setting up a relative_sentence_index to capture the location
+            # of each sentence within their respective files
+            relative_sentence_index = 0
+            # iterate over every individual sentence within the cleaned and tokenized sentence
+            # (each sentence in a single document)
+            for clean_tokenized_sentence in cleaned_tokenized_sentences:
+                # creates a tuple of all the information we'd like to see in the dataframe
+                tupled_files = (filename, clean_tokenized_sentence, relative_sentence_index, index)
+                # adds them to our empty list above (cleaned_texts)
+                cleaned_texts.append(tupled_files)
+                # increments the relative sentence index
+                relative_sentence_index += 1
+            count += 1
+        progress(100, 100)
     # each tuple element is assigned as a value in the empty dictionary defined above
     cleaned_corpus_as_dictionary['file_names'] = [x[0] for x in cleaned_texts]
     cleaned_corpus_as_dictionary['sentences'] = [x[1] for x in cleaned_texts]
     cleaned_corpus_as_dictionary['relative_sentence_index'] = [x[2] for x in cleaned_texts]
+    cleaned_corpus_as_dictionary['newspaper_0_novel_1'] = [x[3] for x in cleaned_texts]
+
     # create df from dictionary
     df = pd.DataFrame(cleaned_corpus_as_dictionary)
+    
     print("\n[-] Text preprocessing completed.")
     return df
